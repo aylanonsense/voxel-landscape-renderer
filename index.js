c5cedb59c67c9e7ac2d4d8e00bdc2c149fdf13f4
bridgs <bridgs.dev@gmail.com>
2018-05-30 17:44:56 -0400

    Cleaning up unused code + files

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 // serve the web files
-app.use(express.static(__dirname + '/public', { index:false, extensions: ['html'] }));
+app.use(express.static(__dirname + '/public'));
 
 // run the server
 app.listen(process.env.PORT || 3000);
